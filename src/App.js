c8e20eaf17d3494f68179aa3cd8bdafd4183fb5e
Chris Young <chris@chris-young.com>
2019-02-16 13:08:56 +1100

    Create React App

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './goldfish.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Goldfish
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
