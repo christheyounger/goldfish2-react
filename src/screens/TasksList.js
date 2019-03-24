273d07ba78c9cc2773620d2166b67c081c06e456
Chris Young <chris@chris-young.com>
2019-03-24 21:28:12 +1100

    Proper add task component, api call

--- src/screens/TasksList.js
+++ src/screens/TasksList.js
@@ -1,17 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import TasksListTask from '../component/TasksListTask';
+import TaskNew from '../component/TaskNew';
 
 export default function TasksList(props) {
 
     const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState(null);
     useEffect(() => {
         if (!tasks.length) fetch('http://localhost:8000/api/tasks')
         .then(response => response.json())
         .then(setTasks)
-    })
+    });
 
-    function addTask() {
-        setTasks(tasks.concat({}));
+    async function addTask (task) {
+        await fetch('http://localhost:8000/api/tasks', {
+            body: JSON.stringify(task),
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'}
+        });
+        setTasks(tasks.concat(task));
     }
 
     return (
@@ -21,9 +28,10 @@ export default function TasksList(props) {
                     <TasksListTask key={task.id} task={task} />
                 ))}
             </ol>
-            <button onClick={() => {addTask()}}>
+            <button onClick={() => {setNewTask({})}}>
                 Hello
             </button>
+            {newTask && (<TaskNew add={addTask}/>)}
         </div>
     )
 }
