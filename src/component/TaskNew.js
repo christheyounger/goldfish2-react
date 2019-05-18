0edde9bccd149af7db02142cee5f3cfe36673f10
Chris Young <chris@chris-young.com>
2019-05-18 16:00:05 +1000

    Format with prettier

--- src/component/TaskNew.js
+++ src/component/TaskNew.js
@@ -1,14 +1,26 @@
-import React, {useState} from 'react';  
+import React, { useState } from "react";
 
 export default function TaskNew(props) {
-    const [title, setTitle] = useState('');
-    return (
-        <article>
-            <h2>New Task:</h2>
-            <form onSubmit={(e) => {e.preventDefault(); props.add({title})}}>
-            <input type="text" name="title" value={title} onChange={(e) => { setTitle(e.target.value) }}/>
-            <button type="submit">Add</button>
-            </form>
-        </article>
-    )
+  const [title, setTitle] = useState("");
+  return (
+    <article>
+      <h2>New Task:</h2>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          props.add({ title });
+        }}
+      >
+        <input
+          type="text"
+          name="title"
+          value={title}
+          onChange={e => {
+            setTitle(e.target.value);
+          }}
+        />
+        <button type="submit">Add</button>
+      </form>
+    </article>
+  );
 }
--- src/screens/TasksList.js
+++ src/screens/TasksList.js
@@ -1,37 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import TasksListTask from '../component/TasksListTask';
-import TaskNew from '../component/TaskNew';
+import React, { useEffect, useState } from "react";
+import TasksListTask from "../component/TasksListTask";
+import TaskNew from "../component/TaskNew";
 
 export default function TasksList(props) {
-
-    const [tasks, setTasks] = useState([]);
-    const [newTask, setNewTask] = useState(null);
-    useEffect(() => {
-        if (!tasks.length) fetch('http://localhost:8000/api/tasks')
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState(null);
+  useEffect(() => {
+    if (!tasks.length)
+      fetch("http://localhost:8000/api/tasks")
         .then(response => response.json())
-        .then(setTasks)
+        .then(setTasks);
+  });
+
+  async function addTask(task) {
+    await fetch("http://localhost:8000/api/tasks", {
+      body: JSON.stringify(task),
+      method: "POST",
+      headers: { "Content-Type": "application/json" }
     });
+    setTasks(tasks.concat(task));
+  }
 
-    async function addTask (task) {
-        await fetch('http://localhost:8000/api/tasks', {
-            body: JSON.stringify(task),
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'}
-        });
-        setTasks(tasks.concat(task));
-    }
-
-    return (
-        <div>
-            <ol>
-                {tasks.map(task => (
-                    <TasksListTask key={task.id} task={task} />
-                ))}
-            </ol>
-            <button onClick={() => {setNewTask({})}}>
-                Hello
-            </button>
-            {newTask && (<TaskNew add={addTask}/>)}
-        </div>
-    )
+  return (
+    <div>
+      <ol>
+        {tasks.map(task => (
+          <TasksListTask key={task.id} task={task} />
+        ))}
+      </ol>
+      <button
+        onClick={() => {
+          setNewTask({});
+        }}
+      >
+        Hello
+      </button>
+      {newTask && <TaskNew add={addTask} />}
+    </div>
+  );
 }
