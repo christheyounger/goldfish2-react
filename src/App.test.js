ed5f2b81aa6f5ae06cbdab23e9424c222feb1fed
Chris Young <chris@chris-young.com>
2019-05-19 21:34:38 +1000

    Test snapshot

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import render from "react-test-renderer";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it("renders without crashing", () => {
+  const wrapper = render.create(<App />);
+  expect(wrapper.toJSON()).toMatchSnapshot();
 });
